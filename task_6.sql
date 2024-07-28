602dd87f183cf62c4f04f65a225c95cca3cb188d
mmanape-l <mmanape.letlalo@gmail.com>
2024-07-28 18:46:53 +0200

    Add files via upload

---
+++ task_6.sql
@@ -0,0 +1,12 @@
+-- Switch to the provided database
+USE alx_book_store;
+
+-- Insert multiple rows into the customer table using separate INSERT statements
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.');
+
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
