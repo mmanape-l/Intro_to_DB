96cf5a0a3cb62bb6599c9932d5577d6bf4fbb3a4
mmanape-l <mmanape.letlalo@gmail.com>
2024-07-29 14:44:49 +0200

    Add files via upload

---
+++ task_2.sql
@@ -0,0 +1,56 @@
+-- Use the existing database
+USE alx_book_store;
+
+-- Drop tables if they exist to avoid conflicts
+DROP TABLE IF EXISTS Order_details;
+DROP TABLE IF EXISTS Orders;
+DROP TABLE IF EXISTS Books;
+DROP TABLE IF EXISTS Authors;
+DROP TABLE IF EXISTS Customers;
+
+-- Create the 'Authors' table
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(255) NOT NULL
+);
+
+-- Create the 'Books' table
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author_id INT NOT NULL,
+    publication_year YEAR,
+    price DECIMAL(10, 2),
+    stock_quantity INT,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create the 'Customers' table
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) UNIQUE NOT NULL,
+    phone VARCHAR(20),
+    address TEXT
+);
+
+-- Create the 'Orders' table with foreign key constraint
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    status VARCHAR(50) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create the 'Order_details' table
+CREATE TABLE Order_details (
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
+
